936c294500584ea75ab7ddbff0fc898b9f9086f4
Yinrong Wu <wu.yinrong@wustl.edu>
2019-02-25 10:22:15 -0600

    commit the final verison of Event+

---
+++ src/db/DBConnectionFactory.java
@@ -0,0 +1,25 @@
+package db;
+
+import db.mysql.MySQLConnection;
+
+public class DBConnectionFactory {
+	// This should change based on the database.
+	private static final String DEFAULT_DB = "mysql";
+	
+	public static DBConnection getDBConnection(String db) {
+		switch (db) {
+		case "mysql":
+			return new MySQLConnection();
+		case "mongodb":
+			//return MongoDBConnection.getInstance();
+			return null;
+		// You may try other dbs and add them here.
+		default:
+			throw new IllegalArgumentException("Invalid db " + db);
+		}
+	}
+	
+	public static DBConnection getConnection() {
+		return getDBConnection(DEFAULT_DB);
+	}
+}
